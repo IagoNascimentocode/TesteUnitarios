c824f9ea1f17f5e4b0e4d20280b7bdcdb0872ca3
IagoNascimentocode <iago_ns@hotmail.com>
2021-05-10 13:28:35 -0300

    Test - CreateStatement

---
+++ src/modules/statements/useCases/createStatement/CreateStatement.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase"
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
+import { ICreateStatementDTO } from "./ICreateStatementDTO";
+
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+
+
+describe("Deposit balance", () => {
+
+ beforeEach(() => {
+  inMemoryUsersRepository = new InMemoryUsersRepository();
+  inMemoryStatementsRepository = new InMemoryStatementsRepository();
+  createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository);
+ });
+
+ it("Should be able possible to make a deposit", async () => {
+
+  const user = await createUserUseCase.execute({
+   name: "User test",
+   email: "usertest@algumacoisa.com",
+   password: "12345"
+  });
+
+  const statement = {
+   user_id: user.id,
+   type: "deposit",
+   amount: 100,
+   description: "descriprtion"
+  } as ICreateStatementDTO
+
+  const response = await createStatementUseCase.execute(statement)
+
+  expect(response).toHaveProperty("id")
+
+ })
+})
